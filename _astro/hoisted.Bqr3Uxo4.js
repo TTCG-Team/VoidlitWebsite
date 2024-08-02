ab28532a8511cc5874fe34420d1e487d79234a01
Gen <gsioteam@protonmail.com>
2024-08-02 17:26:45 +0800

    Fixed a display problem.

---
+++ _astro/hoisted.Bqr3Uxo4.js
@@ -0,0 +1,1 @@
+import{W as t}from"./WalletButton.ClAvDEai.js";import{c as o}from"./solana-wallets-vue.DrfEbcIT.js";/* empty css                                   */import"./_commonjsHelpers.BosuxZz1.js";o(t).mount("#wallet-button");
