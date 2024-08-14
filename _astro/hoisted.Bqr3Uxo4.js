becc33b8aef96e8be3126d6016bb7a9634edd25f
Gen <gsioteam@protonmail.com>
2024-08-14 15:30:38 +0800

    Update for mining program

--- _astro/hoisted.Bqr3Uxo4.js
+++
@@ -1,1 +0,0 @@
-import{W as t}from"./WalletButton.ClAvDEai.js";import{c as o}from"./solana-wallets-vue.DrfEbcIT.js";/* empty css                                   */import"./_commonjsHelpers.BosuxZz1.js";o(t).mount("#wallet-button");
